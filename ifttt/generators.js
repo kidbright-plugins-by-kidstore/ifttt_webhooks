ccd9c97f6a5b236fc70dfbd22067c77db12a2a7d
Max <max30012540@hotmail.com>
2019-02-20 19:14:57 +0700

    แก้บัคไม่ใส่ค่า value1 value2 value3 แล้วทำงานไม่ได้

--- ifttt/generators.js
+++ ifttt/generators.js
@@ -4,6 +4,10 @@ Blockly.JavaScript['ifttt_webhooks'] = function(block) {
 	var value_value1 = Blockly.JavaScript.valueToCode(block, 'value1', Blockly.JavaScript.ORDER_ATOMIC);
 	var value_value2 = Blockly.JavaScript.valueToCode(block, 'value2', Blockly.JavaScript.ORDER_ATOMIC);
 	var value_value3 = Blockly.JavaScript.valueToCode(block, 'value3', Blockly.JavaScript.ORDER_ATOMIC);
+	if (value_value1.length === 0) value_value1 = '(char*)""';
+	if (value_value2.length === 0) value_value2 = '(char*)""';
+	if (value_value3.length === 0) value_value3 = '(char*)""';
+	
 	var code = 'DEV_IO.IFTTT().set(1, ' + value_value1 + '); DEV_IO.IFTTT().set(2, ' + value_value2 + '); DEV_IO.IFTTT().set(3, ' + value_value3 + '); DEV_IO.IFTTT().send(' + value_key + ', ' + value_event + ');\n';
 	return code;
 };
